64a5edd36ebd1e982b71c61c9af2910db4fed124
Little Foot <littlefoot@mailfence.com>
2025-05-04 23:08:13 -0700

    Refactor ZSH dot files into subdirectories

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -1,12 +1,15 @@
 if [[ -z "$ZSH_DOTFILES" ]]; then
   export ZSH_DOTFILES=$(pwd)
 fi
+ZSH_DOTFILES_CORE=$ZSH_DOTFILES/core
+ZSH_DOTFILES_THEMES=$ZSH_DOTFILES/themes
 ZSH_THEME="p10k"
 #ZSH_THEME="starship"
 
-source $ZSH_DOTFILES/exports.zsh
-source $ZSH_DOTFILES/aliases.zsh
-source $ZSH_DOTFILES/functions.zsh
+# Source the exports first to avoid dependency issues
+source $ZSH_DOTFILES_CORE/exports.zsh
+source $ZSH_DOTFILES_CORE/aliases.zsh
+source $ZSH_DOTFILES_CORE/functions.zsh
 if [[ "$ZSH_THEME" == *"p10k"* ]]; then
   # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
   # Initialization code that may require console input (password prompts, [y/n]
@@ -15,9 +18,9 @@ if [[ "$ZSH_THEME" == *"p10k"* ]]; then
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
   fi
 
-  source $ZSH_DOTFILES/ohmyzsh.zsh
+  source $ZSH_DOTFILES_THEMES/ohmyzsh.zsh
   # To customize prompt, run `p10k configure` or edit ~/.config/dotfiles/zsh/p10k.zsh.
-  [[ ! -f ~/.config/dotfiles/zsh/p10k.zsh ]] || source $ZSH_DOTFILES/p10k.zsh
+  [[ ! -f $ZSH_DOTFILES_THEMES/p10k.zsh ]] || source $ZSH_DOTFILES_THEMES/p10k.zsh
 elif [[ "$ZSH_THEME" == *"starship"* ]]; then
   eval "$(starship init zsh)"
 fi
