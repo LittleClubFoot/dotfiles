a998ded6f1ce4bc647c5ebfc510b710c19ccf483
Little Foot <littlefoot@mailfence.com>
2025-05-04 23:21:45 -0700

    Add navigation aliases to ZSH

---
+++ zsh/core/aliases.zsh
@@ -0,0 +1,16 @@
+# Navigation
+alias cd-nixos="$CONFIG/nixos-config"
+alias cd-nvim="$CONFIG/nvim"
+alias cd-dotfiles="$CONFIG_DOTFILES"
+alias cd-zsh="$CONFIG_DOTFILES/zsh"
+alias cd-i3="$CONFIG_DOTFILES/i3"
+alias cd-i3status="$CONFIG_DOTFILES/i3status"
+
+# Nvim
+alias nvim-nixos="cd-nixos; nvim ."
+alias nvim-nvim="cd-nvim; nvim ."
+alias nvim-dotfiles="cd-dotfiles; nvim ."
+alias nvim-zsh="cd-zsh; nvim ."
+alias nvim-i3="cd-i3; nvim ."
+alias nvim-i3status="cd-i3status; nvim ."
+
