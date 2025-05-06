e2c8ac57e4c0ef32a07b7a02a1810f96844644f8
LittleClubFoot <littlefoot@mailfence.com>
2025-05-06 10:01:28 -0700

    Add functions to set the Git user config for LittleClubFoot

--- zsh/core/functions.zsh
+++ zsh/core/functions.zsh
@@ -1,6 +1,18 @@
+# Config shortcuts
 config-zsh () {
   cd $ZSH_DOTFILES
   nvim .
   cd -
   source $HOME/.zshrc
+}
+
+# Git
+git-update-config-littlefoot () {
+  git config user.name "LittleClubFoot"
+  git config user.email "littlefoot@mailfence.com"
+}
+
+git-update-config-littlefoot-global () {
+  git config --global user.name "LittleClubFoot"
+  git config --global user.email "littlefoot@mailfence.com"
 }
