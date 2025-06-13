ddd994fa89c1b48124c505d22ffdd465d03ea4d2
LittleClubFoot <littlefoot@mailfence.com>
2025-06-13 08:29:33 -0700

    Add pomo() funciton

--- zsh/core/functions.zsh
+++ zsh/core/functions.zsh
@@ -16,3 +16,18 @@ git-update-config-littlefoot-global () {
   git config --global user.name "LittleClubFoot"
   git config --global user.email "littlefoot@mailfence.com"
 }
+
+# Workflows
+pomo() {
+    arg1=$1
+    shift
+    args="$*"
+
+    min=${arg1:?Example: pomo 15 Take a break}
+    sec=$((min * 60))
+    msg="${args:?Example: pomo 15 Take a break}"
+
+    while true; do
+        sleep "${sec:?}" && echo "${msg:?}" && notify-send -u critical -t 0 "${msg:?}"
+    done
+}
